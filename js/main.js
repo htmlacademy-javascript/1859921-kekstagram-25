14e4cae31d0b70a5bdba3db592b395ef0d7a7aed
EkaterinaDmitr <ketrins-lite_94@mail.ru>
2022-03-09 21:38:24 +0300

    ДЗ Модуляция

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-import './data';
-import './util';
+/* eslint-disable no-console */
+// eslint-disable-next-line no-unused-vars
+import {randomUserPhotos} from './util';
+
+// eslint-disable-next-line eol-last
+console.log(randomUserPhotos);
--- js/util.js
+++ js/util.js
@@ -1,8 +1,17 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
+import {  USER_NAMES,
+  COMMENTS_TEXT,
+  PHOTO_DESCRIPTIONS,
+  PHOTOS_COUNT,
+  MIN_LIKES_COUNT,
+  MAX_LIKES_COUNT,
+  getRandomArrayElement
+} from './data';
+
 const createCommentsPhoto = (id) => ({
   id,
-  avatar: `img/avatar/${getRandomPositiveInteger(1, PHOTO_DESCRIPTIONS.length + 1)}.svg`,
+  avatar: `img/avatar/${getRandomPositiveInteger(1, 6)}.svg`,
   name: getRandomArrayElement(USER_NAMES),
   message: getRandomArrayElement(COMMENTS_TEXT),
 });
@@ -19,7 +28,5 @@ const randomUserPhotos = Array.from({length: PHOTOS_COUNT}, (elem, index) => cre
 console.log(randomUserPhotos);
 
 export {
-  createCommentsPhoto,
-  createPhotos,
   randomUserPhotos,
 };
