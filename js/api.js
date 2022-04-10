172f8bcd89708176a1c1b398b44ce1e62a99e18d
EkaterinaDmitr <ketrins-lite_94@mail.ru>
2022-04-10 20:52:13 +0300

    правки/Надо подкачаться

--- js/api.js
+++ js/api.js
@@ -1,22 +1,60 @@
-const getData = (onSuccess) => {
-  fetch('https://25.javascript.pages.academy/kekstagram/data')
-    .then((response) => response.json())
-    .then((photos) => {
-      onSuccess(photos);
-    })
-    .catch();
+import {renderMiniatures} from './thumbnail-rendering.js';
+
+const ALERT_SHOW_TIME = 5000;
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
 };
 
+
+const getData = (onSuccsess) => {
+  fetch('https://25.javascript.pages.academy/kekstagram/data')
+    .then((response) => response.json())
+    .then((picture) => {
+      onSuccsess(picture);
+    })
+    .catch(() => {
+      showAlert('Не удалось загрузить фото.');
+    });
+};
+
+getData((picture) => {
+  renderMiniatures(picture);
+});
+
 const sendData = (onSuccess, onFail, body) => {
-  fetch ('https://25.javascript.pages.academy/kekstagram',
+  fetch(
+    'https://25.javascript.pages.academy/kekstagram',
     {
       method: 'POST',
       body,
-    }
+    },
   )
-    .then(() => onSuccess())
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
     .catch(() => {
-      onFail();
+      onFail('Не удалось отправить форму. Попробуйте ещё раз');
     });
 };
 
