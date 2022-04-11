3119e3d524591efd6d1e6942de0b4eac44b449d0
EkaterinaDmitr <ketrins-lite_94@mail.ru>
2022-04-11 21:20:41 +0300

    Перламутровые пуговицы/ ч1

--- js/main.js
+++ js/main.js
@@ -6,6 +6,22 @@ import {showsSuccessMessage} from './upload-message.js';
 import {getData} from './api.js';
 import './scale.js';
 
+import {initFilters, setFilterUpdate} from './filter.js';
+
+import {showAlert, debounce} from './util.js';
+
+const RERENDER_DELAY = 500;
+
+getData((photos) => {
+  renderMiniatures(photos);
+  initFilters();
+  setFilterUpdate(debounce(
+    () => renderMiniatures(photos),
+    RERENDER_DELAY,
+  ));
+}, showAlert);
+setUserFormSubmit();
+
 getData ((photos) => {
   renderMiniatures(photos);
 });
--- js/thumbnail-rendering.js
+++ js/thumbnail-rendering.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-undef */
 import {showBigPicture} from './popup.js';
+import {getRandomPositiveInteger} from './util.js';
 
 const picturesList = document.querySelector('.pictures');
 picturesList.querySelector('.pictures__title').classList.remove('visually-hidden');
@@ -8,19 +8,50 @@ const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
+let imagesCount = 25;
+
+const sortPhotos = (photoA, photoB) => {
+  const activeFilter = document.querySelector('.img-filters__button--active').id;
+  imagesCount = 25;
+
+  switch (activeFilter) {
+    case 'filter-discussed':
+      return photoB.comments.length - photoA.comments.length;
+    case 'filter-random':
+      imagesCount = 10;
+      return getRandomPositiveInteger(1, 10) - getRandomPositiveInteger(1, 10);
+    case 'filter-default':
+    default:
+      return 0;
+  }
+};
+
+const clearPhotos = () => {
+  const photos = document.querySelectorAll('.picture');
+  photos.forEach((photo) => {
+    photo.parentNode.removeChild(photo);
+  });
+};
+
 const renderMiniatures = (thumbnailPhotos) => {
+  clearPhotos();
   const picturesThumbnailFragment = document.createDocumentFragment();
 
-  thumbnailPhotos.forEach((thumbnailPhoto) => {
-    const {url, likes, comments} = thumbnailPhoto;
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    picturesThumbnailFragment.appendChild(pictureElement);
-    pictureElement.addEventListener('click', () => showBigPicture(thumbnailPhoto));
-
-  });
+  thumbnailPhotos
+    .slice()
+    .sort(sortPhotos)
+    .slice(0, imagesCount)
+    .forEach((thumbnailPhoto) => {
+      const {url, likes, comments} = thumbnailPhoto;
+      const pictureElement = pictureTemplate.cloneNode(true);
+      pictureElement.querySelector('.picture__img').src = url;
+      pictureElement.querySelector('.picture__likes').textContent = likes;
+      pictureElement.querySelector('.picture__comments').textContent = comments.length;
+      pictureElement.addEventListener('click', () => {
+        showBigPicture(thumbnailPhoto);
+      });
+      picturesThumbnailFragment.appendChild(pictureElement);
+    });
   picturesList.appendChild(picturesThumbnailFragment);
 };
 
